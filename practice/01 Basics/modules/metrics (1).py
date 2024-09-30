c1d8f77a1bd24a24463bb813edfc034522131ff1
igor-sharipov1 <70577702+igor-sharipov1@users.noreply.github.com>
2024-09-30 17:57:41 +0500

    Add files via upload

---
+++ practice/01 Basics/modules/metrics (1).py
@@ -0,0 +1,95 @@
+import numpy as np
+
+
+def ED_distance(ts1: np.ndarray, ts2: np.ndarray) -> float:
+    """
+    Calculate the Euclidean distance
+
+    Parameters
+    ----------
+    ts1: the first time series
+    ts2: the second time series
+
+    Returns
+    -------
+    ed_dist: euclidean distance between ts1 and ts2
+    """
+    
+    ed_dist = 0
+
+    # INSERT YOUR CODE
+
+    ed_dist = np.sqrt(np.sum((ts1 - ts2)**2))
+
+    return ed_dist
+
+
+def norm_ED_distance(ts1: np.ndarray, ts2: np.ndarray) -> float:
+    """
+    Calculate the normalized Euclidean distance
+
+    Parameters
+    ----------
+    ts1: the first time series
+    ts2: the second time series
+
+    Returns
+    -------
+    norm_ed_dist: normalized Euclidean distance between ts1 and ts2s
+    """
+
+    n = len(ts1)
+    
+    # Вычисление среднего арифметического и стандартного отклонения для каждого ряда
+    mu_ts1 = np.mean(ts1)
+    mu_ts2 = np.mean(ts2)
+    sigma_ts1 = np.std(ts1)
+    sigma_ts2 = np.std(ts2)
+    
+    # Вычисление скалярного произведения
+    dot_product = np.dot(ts1, ts2)
+    
+    # Вычисление нормализованного евклидова расстояния
+    norm_ed_dist = np.sqrt(np.abs(2 * n * (1 - (dot_product - n * mu_ts1 * mu_ts2) / (n * sigma_ts1 * sigma_ts2))))
+    
+    return norm_ed_dist
+
+
+def DTW_distance(ts1: np.ndarray, ts2: np.ndarray, r: float = 1) -> float:
+    """
+    Calculate DTW distance
+
+    Parameters
+    ----------
+    ts1: first time series
+    ts2: second time series
+    r: warping window size
+    
+    Returns
+    -------
+    dtw_dist: DTW distance between ts1 and ts2
+    """
+
+    dtw_dist = 0
+
+    # INSERT YOUR CODE
+
+    n = len(ts1)
+    d = np.zeros((n+1, n+1))
+
+    for i in range(n+1):
+      for j in range(n+1):
+        if i == 0 and j == 0:
+          d[i, j] = 0
+
+        elif i == 0 or j == 0:
+          d[i, j] = np.inf
+
+        else:
+          d[i, j] = ((ts1[i - 1] - ts2[j - 1])**2 
+          + min(d[i-1, j], d[i, j-1], d[i-1, j-1])
+          )
+    
+    dtw_dist = d[n, n]
+
+    return dtw_dist
---
+++ practice/01 Basics/modules/pairwise_distance (1).py
@@ -0,0 +1,96 @@
+import numpy as np
+
+from modules.metrics import ED_distance, norm_ED_distance, DTW_distance
+from modules.utils import z_normalize
+from sktime.distances import distance
+
+
+class PairwiseDistance:
+    """
+    Distance matrix between time series 
+
+    Parameters
+    ----------
+    metric: distance metric between two time series
+            Options: {euclidean, dtw}
+    is_normalize: normalize or not time series
+    """
+
+    def __init__(self, metric: str = 'euclidean', is_normalize: bool = False) -> None:
+
+        self.metric: str = metric
+        self.is_normalize: bool = is_normalize
+    
+
+    @property
+    def distance_metric(self) -> str:
+        """Return the distance metric
+
+        Returns
+        -------
+            string with metric which is used to calculate distances between set of time series
+        """
+
+        norm_str = ""
+        if (self.is_normalize):
+            norm_str = "normalized "
+        else:
+            norm_str = "non-normalized "
+
+        return norm_str + self.metric + " distance"
+
+
+    def _choose_distance(self):
+        """ Choose distance function for calculation of matrix
+        
+        Returns
+        -------
+        dict_func: function reference
+        """
+
+        dist_func = None
+        
+        if self.metric == 'euclidean':
+          dist_func = ED_distance
+        elif self.metric == 'dtw':
+          dist_func = DTW_distance
+        elif self.metric == 'norm_euclidean':
+          dist_func = norm_ED_distance
+
+        return dist_func
+
+
+    def calculate(self, input_data: np.ndarray) -> np.ndarray:
+        """ Calculate distance matrix
+        
+        Parameters
+        ----------
+        input_data: time series set
+        
+        Returns
+        -------
+        matrix_values: distance matrix
+        """
+        
+        matrix_shape = (input_data.shape[0], input_data.shape[0])
+        matrix_values = np.zeros(shape=matrix_shape)
+        
+        # INSERT YOUR CODE
+
+        if self.is_normalize and self.metric != 'euclidean':
+          input_data = z_normalize(input_data)
+        
+        dist_func = self._choose_distance()
+        
+        K = input_data.shape[0]
+
+        for i in range(K):
+          for j in range(i,K):
+            if i == j:
+              matrix_values[i, j] = 0
+              continue
+
+            matrix_values[i,j] = dist_func(input_data[i], input_data[j])
+            matrix_values[j,i] = matrix_values[i,j]
+
+        return matrix_values
